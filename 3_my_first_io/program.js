594f20ddf22510b744781b07a2c52eb12d4ba116
Michal Vich <vich@zensys.cz>
2014-08-14 17:00:52 +0200

    solution for my first async

--- 3_my_first_io/program.js
+++ 3_my_first_io/program.js
@@ -1,6 +1,6 @@
-var fs = require('fs')
+var fs = require('fs');
 
-var filename = process.argv[2]
-var data = fs.readFileSync(filename)
-var lines = data.toString().split('\n').length-1
-console.log(lines)
+var filename = process.argv[2];
+var data = fs.readFileSync(filename);
+var lines = data.toString().split('\n').length-1;
+console.log(lines);
