29ae8a17756c769635366426a1e3906f163365b6
Michal Vich <vich@zensys.cz>
2014-08-14 15:15:10 +0200

    solution for my first io

--- 3_my_first_io/program.js
+++ 3_my_first_io/program.js
@@ -2,5 +2,5 @@ var fs = require('fs')
 
 var filename = process.argv[2]
 var data = fs.readFileSync(filename)
-var lines = data.toString().split('\n').length-1;
-console.log(lines);
+var lines = data.toString().split('\n').length-1
+console.log(lines)
