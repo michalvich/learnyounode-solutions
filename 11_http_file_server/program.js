bdfdf4ba696744b0db40eeb7125e4f032037ffa4
Michal Vich <vich@zensys.cz>
2014-08-15 23:19:26 +0200

    solution for http file server

---
+++ 11_http_file_server/program.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+var filename = process.argv[3];
+
+var server = http.createServer(function (request, response) {
+  fs.createReadStream(filename).pipe(response);
+});
+
+server.listen(process.argv[2]);
